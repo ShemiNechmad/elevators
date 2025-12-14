5e748b2fc6ddbe466660580413defc544add1303
Shemi <scaleterschoice@gmail.com>
2025-12-14 09:46:41 +0200

    sub title

--- app/building/layout.tsx
+++ app/building/layout.tsx
@@ -9,7 +9,8 @@ export default function BuildingLayout({ children }: BuildingLayoutProps) {
     return (
         <div className="h-screen w-screen overflow-hidden bg-[#EEEEEE] flex flex-col items-center pb-[30px]">
 
-            <div className="text-2xl text-center my-18 font-['Courier_New',_Courier,_monospace, bold]">Elevator Exercise</div>
+            <div className="text-2xl text-center mt-15 font-['Courier_New',_Courier,_monospace, bold]">Elevator Exercise</div>
+            <div className="text-center mb-2 font-['Courier_New',Courier,monospace, bold]">(5 sec. per floor + 2 sec.)</div>
 
             <main className="h-full overflow-hidden">{children}</main>
 
